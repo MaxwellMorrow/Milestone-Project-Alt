166a59f806bff8aac89e911fea54eb4a740a243a
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-02-14 17:00:57 -0800

    finished CSS adding JS for functionality

--- Script.js
+++ Script.js
@@ -1,11 +1,10 @@
 
 // selecting our tic tac toe squares to alter in javascript 
-let square1 = document.querySelector("#square1")
-let square2 = document.querySelector("#square2")
-let square3 = document.querySelector("#square3")
-let square4 = document.querySelector("#square4")
-let square5 = document.querySelector("#square5")
-let square6 = document.querySelector("#square6")
-let square7 = document.querySelector("#square7")
-let square8 = document.querySelector("#square8")
-let square9 = document.querySelector("#square9")
+const squareElements = document.querySelectorAll(".square")
+
+// these variables we will use to apply CSS to each square by changing its class name
+const X_CLASS = "x"
+const CIRCLE_CLASS = "circle"
+
+let xturn 
+
