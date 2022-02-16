227f16c648436760d803f721a2313107927462b0
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-02-15 18:23:25 -0800

    added files for 4x4 game and comments to 3x3 game

--- Script.js
+++ Script.js
@@ -20,16 +20,18 @@ const WIN_CONDITIONS = [
     [2,4,6]
 ]
 
-
+// we must establish whos turn it is first before create additional functionality
 let xturn 
 
+// Adds an event listener to every square in square elements also lets them only be clicked once
 squareElements.forEach(square =>{
     square.addEventListener("click", squareClick, {once: true})
 
 })
-
+// when we click a square we place a mark, check for win and swap turns 
 function squareClick(e){
     const square = e.target
+    // this is a ternary first checks for if xturn is true if not then it uses Circle class 
     const currentClass = xturn ? X_CLASS : CIRCLE_CLASS
     placeMark(square, currentClass)
     if(winCheck(currentClass)){
@@ -38,7 +40,7 @@ function squareClick(e){
     turnSwap()
 
 }
-
+// To add our mark we only need to change our class on the square we click on to the mark we want
 function placeMark (square, currentClass){
     square.classList.add(currentClass)
 }
@@ -47,6 +49,7 @@ function turnSwap(){
     //  this simply swaps from one turn to the other by setting x turn to not x turn flip flopping it back and fourth
     xturn = !xturn
 }
+
 function winCheck(currentClass){
     // checking all the win conditions in the array if .some returns true if ANY win conditions are met
     return WIN_CONDITIONS.some(combination => {
